6227a2fd47265f46c6e7ed4888b645493f21473a
upupnoah <upupqi.cs@gmail.com>
2024-08-04 00:14:32 +0700

    feat: complete vecs & move_semantics

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,6 +4,7 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        output.push(element * 2);
     }
 
     output
@@ -25,6 +26,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
         .iter()
         .map(|element| {
             // ???
+            element * 2
         })
         .collect()
 }
--- exercises/06_move_semantics/move_semantics5.rs
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -4,21 +4,21 @@
 // removing references (the character `&`).
 
 // Shouldn't take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = data.to_uppercase();
+fn string_uppercase(data: &mut String) {
+    *data = data.to_uppercase();
 
     println!("{data}");
 }
 
 fn main() {
-    let data = "Rust is great!".to_string();
+    let mut data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(&mut data);
 }
