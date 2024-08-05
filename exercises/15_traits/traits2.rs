bb3da94601bfe52dbab5b0bb633f0d41b9b322c9
upupnoah <upupqi.cs@gmail.com>
2024-08-05 09:03:50 +0700

    feat(traits): complete traits

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -4,6 +4,13 @@ trait AppendBar {
 
 // TODO: Implement the trait `AppendBar` for a vector of strings.
 // `append_bar` should push the string "Bar" into the vector.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut new_vec = self;
+        new_vec.push("Bar".into());
+        new_vec
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
